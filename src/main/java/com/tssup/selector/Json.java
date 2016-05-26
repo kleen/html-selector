cb60b5a36b138e8968515c0c49bda9d7eca2661e
kleen <hx273@tssup.com>
2016-05-26 12:18:50 +0800

    init

---
+++ src/main/java/com/tssup/selector/Json.java
@@ -0,0 +1,56 @@
+package com.tssup.selector;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.List;
+
+/**
+ * parse json
+ * @author kleen888@gmail.com
+ * @since 0.2.0
+ */
+public class Json extends PlainText {
+
+    public Json(List<String> strings) {
+        super(strings);
+    }
+
+    public Json(String text) {
+        super(text);
+    }
+
+//    /**
+//     * remove padding for JSONP
+//     * @param padding
+//     * @return
+//     */
+//    public Json removePadding(String padding) {
+//        String text = getFirstSourceText();
+//        XTokenQueue tokenQueue = new XTokenQueue(text);
+//        tokenQueue.consumeWhitespace();
+//        tokenQueue.consume(padding);
+//        tokenQueue.consumeWhitespace();
+//        String chompBalanced = tokenQueue.chompBalancedNotInQuotes('(', ')');
+//        return new Json(chompBalanced);
+//    }
+
+    public <T> T toObject(Class<T> clazz) {
+        if (getFirstSourceText() == null) {
+            return null;
+        }
+        return JSON.parseObject(getFirstSourceText(), clazz);
+    }
+
+    public <T> List<T> toList(Class<T> clazz) {
+        if (getFirstSourceText() == null) {
+            return null;
+        }
+        return JSON.parseArray(getFirstSourceText(), clazz);
+    }
+
+    @Override
+    public Selectable jsonPath(String jsonPath) {
+        JsonPathSelector jsonPathSelector = new JsonPathSelector(jsonPath);
+        return selectList(jsonPathSelector,getSourceTexts());
+    }
+}
---
+++ src/test/java/com/tssup/selector/AppTest.java
@@ -0,0 +1,44 @@
+package com.tssup.selector;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        String ss = "<div class='notification note-success'><a href='#' class='close'></a>\n" +
+                "            <p>用户已建立。 <a href=\"/iredadmin/create/user/tssup.com\">再添加一个？</a></p>\n" +
+                "        </div>";
+        Html html = new Html(ss);
+        System.out.println(html.$(".notification p").get().contains("用户已建立"));
+
+
+    }
+}
